e20da0468ac4771a83f209b2e540ca37d6797a18
rightpunchChen <rightpunch1123@gmail.com>
2024-12-11 12:41:28 +0800

    Update photomosaic_superpixel_multi.py

--- 313554013/photomosaic_superpixel_multi.py
+++ 313554013/photomosaic_superpixel_multi.py
@@ -5,50 +5,41 @@ import argparse
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-def load_images(folder_path, resize_width, resize_height):
-    resize_image_list = []
-    files = os.listdir(folder_path)  
-    for i in tqdm(files):     
-        image = cv2.imread(os.path.join(folder_path, i))  
-        if image.shape[0]  > image.shape[1]:
-            image = cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        image = cv2.resize(image, (resize_width, resize_height), interpolation=cv2.INTER_LINEAR)
-        resize_image_list.append(image)
-    return resize_image_list
-
-def process_superpixel(idx, target_image, labels, loaded_images, mask, x, y, w, h, files, image_folder):
+def process_superpixel(idx, target_image, labels, mask, x, y, w, h, image_folder):
     part = target_image[labels == idx]
     part_mean = np.mean(part, axis=0)
     min_distance = 195075
 
-    mask_region = mask[y:y+h, x:x+w]
-    mask_region[mask_region > 0] = 1
+    files = os.listdir(opt.image_pool)
+    for i in files:
+        tile = cv2.imread(os.path.join(image_folder, i))
 
-    bgr_mean_list = []
-    for i in loaded_images:
-        i_resized = cv2.resize(i, (w, h), interpolation=cv2.INTER_AREA)
-        i_masked = np.zeros_like(i_resized)
-        for c in range(3):
-            i_masked[:, :, c] = mask_region * i_resized[:, :, c]
-        # bgr_mean_list.append(np.mean(i_masked, axis=(1, 0)))
-        bgr_mean_list.append(np.mean(i_masked[mask_region==1], axis=0))
+        tile_h, tile_w = tile.shape[:2]
+        scale_factor = max(h / tile_h, w / tile_w)
+        new_tile_w = int(tile_w * scale_factor)
+        new_tile_h = int(tile_h * scale_factor)
+        
+        mask_region = mask[y:y+h, x:x+w]
+        mask_region[mask_region > 0] = 1
+        tile_resized = cv2.resize(tile, (new_tile_w, new_tile_h), interpolation=cv2.INTER_AREA)
 
-    image_idx = 0
-    for i, k in enumerate(bgr_mean_list):
-        distance = ((part_mean[0] - k[0]) ** 2 +
-                    (part_mean[1] - k[1]) ** 2 +
-                    (part_mean[2] - k[2]) ** 2)
+        top = new_tile_h // 2 - h // 2
+        bottom = top + h
+        left = new_tile_w // 2 - w // 2
+        right = left + w
+        tile_cropped = tile_resized[top:bottom, left:right]
+
+        bgr_mean = np.mean(tile_cropped[mask_region>0], axis=0)
+        distance = np.sum((part_mean - bgr_mean) ** 2)
         if distance < min_distance:
             min_distance = distance
-            image_idx = i
+            final_tile = tile_cropped  * mask_region[..., None]
+            final_h = h
+            final_w = w
 
-    image_tile = files[image_idx]
-    image_tile = cv2.imread(os.path.join(image_folder, image_tile))
-    image_tile_resized = cv2.resize(image_tile, (w, h), interpolation=cv2.INTER_AREA)
+    return x, y, final_w, final_h, final_tile
 
-    return x, y, w, h, mask_region, image_tile_resized
-
-def create_superpixel_list_new(target_image, loaded_images, image_folder, files, region_size=50, ruler=50):
+def create_superpixel_list_new(target_image, image_folder, region_size=50, ruler=50):
     slic_image = cv2.ximgproc.createSuperpixelSLIC(target_image, algorithm=cv2.ximgproc.SLIC,
                                             region_size=region_size, ruler=ruler)
     slic_image.iterate(10)
@@ -58,7 +49,7 @@ def create_superpixel_list_new(target_image, loaded_images, image_folder, files,
     output_image = np.zeros((target_image.shape[0], target_image.shape[1], 3), np.float64)
     tasks = []
 
-    max_processes = os.cpu_count()
+    max_processes = os.cpu_count() // 2
     with ThreadPoolExecutor(max_workers=max_processes) as executor:
         for idx in tqdm(range(idx_num)):
             mask = np.zeros_like(labels, dtype=np.uint8)
@@ -66,37 +57,24 @@ def create_superpixel_list_new(target_image, loaded_images, image_folder, files,
             x, y, w, h = cv2.boundingRect(mask)
             if w == 0 or h == 0:
                 continue
-            tasks.append(executor.submit(process_superpixel, idx, target_image, labels, loaded_images, mask, x, y, w, h, files, image_folder))
+            tasks.append(executor.submit(process_superpixel, idx, target_image, labels, mask, x, y, w, h, image_folder))
 
         for future in tqdm(as_completed(tasks), total=len(tasks)):
-            x, y, w, h, mask_region, image_tile_resized = future.result()
-            # for c in range(3):
-            #     output_image[y:y+h, x:x+w, c] += mask_region * image_tile_resized[:, :, c]
-            output_image[y:y+h, x:x+w] += mask_region[..., None] * image_tile_resized
+            x, y, w, h, image_tile = future.result()
+            output_image[y:y+h, x:x+w] += image_tile
             
     return output_image.astype(np.uint8)
 
 def main(opt):
     target_image = cv2.imread(opt.target_root)
 
-    try:
-        loaded_data = np.load(opt.npz_dir)
-    except:
-        resize_image_list = load_images(opt.image_pool, resize_width=150, resize_height=100)
-        np.savez(opt.npz_dir, *resize_image_list)
-        loaded_data = np.load(opt.npz_dir)
-
-    loaded_images = [loaded_data[key] for key in loaded_data]
-    
-    files = os.listdir(opt.image_pool)
-    output_image = create_superpixel_list_new(target_image, loaded_images, opt.image_pool,
-                                                   files, region_size=opt.region_size, ruler=opt.ruler)
+    output_image = create_superpixel_list_new(target_image, opt.image_pool, region_size=opt.region_size, ruler=opt.ruler)
     
     os.makedirs(opt.output_dir, exist_ok=True)
     cv2.imshow('fig', output_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    cv2.imwrite(os.path.join(opt.output_dir, 'output.jpg'), output_image) 
+    cv2.imwrite(os.path.join(opt.output_dir, f'output.jpg'), output_image) 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -105,7 +83,7 @@ if __name__ == "__main__":
     parser.add_argument("--target_root", type=str, help="The path of target image.", default="./test.jpg")
     parser.add_argument("--output_dir", type=str, help="The path of output image.", default="./output")
     parser.add_argument("--region_size", type=int, help="Region size of superpixel.", default=60)
-    parser.add_argument("--ruler", type=int, help="Region ruler of superpixel.", default=80)
+    parser.add_argument("--ruler", type=int, help="Region ruler of superpixel.", default=100)
 
     opt = parser.parse_args()
     main(opt)
